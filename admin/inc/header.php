862b18535f78604514dfb70ef1561f5e9b11fe85
bywilliams <willian.seu@gmail.com>
2023-05-10 19:47:15 -0300

    1.22 - admin login page

---
+++ admin/inc/header.php
@@ -0,0 +1,24 @@
+<?php require_once '../globals.php';?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Controle Financeiro fácil e eficiente é aqui na Finance Control" />
+    <meta name="author" content="William Silva" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/bootstrap.min.css">
+
+    <!-- FontAewsome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+
+    <!-- CSS Local  -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style_adm.css">
+
+    <title>Finance Control Admin</title>
+</head>
+
+
---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<?php require_once("inc/header.php"); ?>
+
+<body>
+    <main>
+        <div class="">
+            <div class="offset-md-4 col-md-4">
+                <div class="container">
+                <h1 class="text-white text-center">Área administrativa</h1>
+                    <img class="" src="<?= $BASE_URL ?>../assets/finance_logo.png" alt="">
+                    
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">E-mail</label>
+                            <input type="email" class="form-control" id="email" placeholder="e-mail:">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Senha</label>
+                            <input type="password" class="form-control" id="password" placeholder="senha">
+                        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-success">Entrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
